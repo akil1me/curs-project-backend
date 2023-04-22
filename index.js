61674763a148105a1b5a8d7d91182cb6c596a43e
akil1me <akipax123@gmail.com>
2023-04-22 14:13:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
